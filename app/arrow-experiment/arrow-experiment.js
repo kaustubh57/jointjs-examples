aa627fe301ce2469d2a9cdbe8a6296b0efec1526
Kaustubh <kaustubh.kesarkar@gmail.com>
2017-04-04 15:51:33 -0400

    Updates the arrow with some more settings.

--- app/arrow-experiment/arrow-experiment.js
+++ app/arrow-experiment/arrow-experiment.js
@@ -21,9 +21,9 @@ joint.shapes.basic.ArrowLinkView = joint.dia.LinkView.extend({
         console.log("******** Custom vertex add method... START ********");
         var vertices = (this.model.get('vertices') || []).slice();
         console.log("Vertex count >>> " + vertices.length);
-        if (vertices.length >= 1) {
-            return;
-        }
+        // if (vertices.length >= 1) {
+        //     return;
+        // }
         joint.shapes.basic.ArrowLinkView.__super__.addVertex.apply(this, arguments);
         console.log("******** Custom vertex add method... END ********");
     }
@@ -38,6 +38,7 @@ var arrowExperiment = function() {
         gridSize: 1,
         model: graph,
         linkView: joint.shapes.basic.ArrowLinkView
+        // interactive: { vertexAdd: false }
     });
 
     paper.on('cell:pointerup', function(cellView) {
@@ -46,6 +47,21 @@ var arrowExperiment = function() {
 
         var halo = new joint.ui.Halo({ graph: graph, paper: paper, cellView: cellView });
         halo.render();
+    });
+
+    paper.on('cell:pointerdblclick', function(cellView, evt) {
+
+        joint.ui.TextEditor.edit(evt.target, {
+            cellView: cellView,
+            // textProperty: 'attrs/text/text'
+            // textProperty: 'content'
+            textProperty: cellView.model instanceof joint.shapes.basic.TextBlock ? 'content' : 'attrs/text/text'
+            // textProperty: cellView.model.isLink() ? 'labels/0/attrs/text/text' : 'content'
+        });
+    });
+
+    paper.on('blank:pointerdown', function(cellView, evt) {
+        joint.ui.TextEditor.close();
     });
 
     var r = new joint.shapes.basic.Rect({
@@ -59,12 +75,48 @@ var arrowExperiment = function() {
     });
 
     var t = new joint.shapes.basic.Text({
-        position: { x: 200, y: 70 },
+        // markup: '<g class="rotatable"><g class="scalable"></g><text/></g>',
+        markup: '<g class="rotatable"><text/></g>',
+        position: { x: 100, y: 70 },
         size: { width: 120, height: 60 },
-        attrs: { text: { text: 'Text', fill: 'black' } }
+        attrs: {
+            text: { text: 'Text5', fill: 'black' },
+            rect: {'fill-opacity':0, stroke:0}
+        }
     });
 
-    graph.addCells([r, c, t]);
+    var textBlock = new joint.shapes.basic.TextBlock({
+        markup: [
+            '<g class="rotatable">',
+            '<g class="scalable"><rect/></g>',
+            '<text class="content"></text>',
+            // '<text></text>',
+            '</g>'
+        ].join(''),
+        position: { x: 200, y: 70 },
+        size: { width: 120, height: 60 },
+        attrs: {
+            rect: {'fill-opacity':0, stroke:0}
+        },
+        content: 'Text Block 11'
+    });
+
+    var l = new joint.dia.Link({
+        labelMarkup: [ // overriding the label markup adding the event attribute
+            '<g class="label">',
+            '<rect event="avoid-vertex-creation"/>',
+            '<text event="avoid-vertex-creation"/>',
+            '</g>'
+        ].join(''),
+        source: { id: r.id },
+        target: { id: c.id },
+        labels: [
+            { position: .5, attrs: { text: { text: 'label' } } }
+        ]
+    });
+
+    // graph.addCells([r, c, t, l]);
+    graph.addCells([t, textBlock]);
 
     $('#graph-json').html(udpateGraphJSONDiv(graph));
 
